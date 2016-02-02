55ca5065fade0220c4ea4072a81bc3645de2f0b3
Balazs Pekar <peba0706@gmail.com>
2016-02-02 19:44:26 +0100

    Adding tweaks and some comments
    Not a huge progress, but better be safe than sorry so this is some sort
    of a backup commit.

--- IdGenerator.java
+++ IdGenerator.java
@@ -4,7 +4,19 @@ public class IdGenerator
 {
 	public static String generate(Product product)
 	{
-		// TODO Auto-generated method stub
-		return null;
+
+		double randNum = (int) (Math.random() * 1001);
+		String result = "";
+		if (product instanceof Movie)
+		{
+			result = "MOV" + randNum;
+		} else if (product instanceof Book)
+		{
+			result = "BOO" + randNum;
+		} else if (product instanceof Game)
+		{
+			result = "GAM" + randNum;
+		}
+		return result;
 	}
 }
--- RentManager.java
+++ RentManager.java
@@ -5,7 +5,15 @@ public class RentManager
 
 	public static void main(String[] args)
 	{
+		// TODO create at least two instances of every type of product
+	}
 
+	public int getBuyableProducts()
+	{
+		// TODO Write a method which gets a List of buyable objects and returns
+		// with the total preferred income by
+		// summarizing the prices of the products in the parameter.
+		return 0;
 	}
 
 }
